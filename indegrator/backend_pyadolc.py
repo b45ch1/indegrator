5690972947b87d6d982a0f17687f53f6555bf484
Sebastian Walter <sebastian.walter@iwr.uni-heidelberg.de>
2014-07-29 13:22:09 +0200

    add fo_reverse to python backend

--- indegrator/backend_pyadolc.py
+++ indegrator/backend_pyadolc.py
@@ -14,7 +14,7 @@ class BackendPyadolc(object):
     def trace(self, dims):
         # trace function
         sys.path.insert(0, self.dir)
-        import bimolkat
+        import ffcn
 
         t = numpy.zeros(1)
         x = numpy.zeros(dims['x'])
@@ -33,7 +33,7 @@ class BackendPyadolc(object):
         adolc.independent(ax)
         adolc.independent(ap)
         adolc.independent(au)
-        bimolkat.ffcn(t, ax, af, ap, au)
+        ffcn.ffcn(t, ax, af, ap, au)
         adolc.dependent(af)
         adolc.trace_off()
         self.traced = True
@@ -46,7 +46,7 @@ class BackendPyadolc(object):
         v[1+x.size+p.size:]         = u
         return v
 
-    def dot_txpu_to_v(self, t_dot, x_dot, p_dot, u_dot):
+    def dot_txpu_to_v(self, t, t_dot, x, x_dot, p, p_dot, u, u_dot):
         nbdirs = t_dot.shape[1]
         v = numpy.zeros((t.size + x.size + p.size + u.size, nbdirs))
         v[0:1]                         = t_dot
@@ -54,6 +54,12 @@ class BackendPyadolc(object):
         v[1+x.size:1+x.size+p.size, :] = p_dot
         v[1+x.size+p.size:, :]         = u_dot
         return v
+
+    def bar_v_to_txpu(self, v, v_bar, x, x_bar, p, p_bar, u, u_bar):
+        # t_bar  += v_bar[0:1]
+        x_bar  += v_bar[1:x.size+1]
+        p_bar  += v_bar[1+x.size:1+x.size+p.size]
+        u_bar  += v_bar[1+x.size+p.size:]
 
     def ffcn(self, t, x, f, p, u):
         if self.traced == False:
@@ -68,12 +74,20 @@ class BackendPyadolc(object):
             self.trace(dims)
 
         v = self.txpu_to_v(t,x,p,u)
-        v_dot = self.dot_txpu_to_v(t_dot, x_dot, p_dot, u_dot)
+        t_dot = numpy.zeros((1, x_dot.shape[1]))
+        v_dot = self.dot_txpu_to_v(t, t_dot, x, x_dot, p, p_dot, u, u_dot)
 
         f[:], f_dot[:,:] = adolc.fov_forward(123, v, v_dot)
 
     def ffcn_bar(self, t, x, x_bar, f, f_bar, p, p_bar, u, u_bar):
-        raise NotImplementedError("")
+        if self.traced == False:
+            dims = {'x': x.size, 'p': p.size, 'u': u.size}
+            self.trace(dims)
+
+        v = self.txpu_to_v(t,x,p,u)
+        adolc.zos_forward(123, v, 1)
+        v_bar = adolc.fos_reverse(123, f_bar)
+        self.bar_v_to_txpu(v, v_bar, x, x_bar, p, p_bar, u, u_bar)
 
     def ffcn_ddot(self, t,
                   x, x_dot2, x_dot1, x_ddot, 
--- indegrator/tests/test_explicit_euler.py
+++ indegrator/tests/test_explicit_euler.py
@@ -4,6 +4,11 @@ import numpy
 
 from indegrator.explicit_euler import ExplicitEuler 
 from indegrator.tapenade import Differentiator 
+from indegrator.backend_fortran import BackendFortran
+from indegrator.backend_pyadolc import BackendPyadolc
+
+
+from numpy.testing import *
 
 
 DIR = os.path.dirname(os.path.abspath(__file__))
@@ -14,7 +19,8 @@ class Test_ExplicitEuler(TestCase):
     def test_forward_vs_reverse(self):
 
         d = Differentiator(os.path.join(DIR, '../../examples/fortran/bimolkat/ffcn.f'))
-        e = ExplicitEuler(os.path.join(DIR, '../../examples/fortran/bimolkat/libproblem.so'))
+        backend_fortran = BackendFortran(os.path.join(DIR, '../../examples/fortran/bimolkat/libproblem.so'))
+        e = ExplicitEuler(backend_fortran)
 
         ts          = numpy.linspace(0,2,50)
         x0          = numpy.ones(5)
@@ -42,6 +48,73 @@ class Test_ExplicitEuler(TestCase):
         b = numpy.sum(xs_bar * e.xs_dot[..., 0])
 
         assert_almost_equal(a, b) 
+
+    def test_fortran_vs_pyadolc_fo_forward(self):
+
+        # d = Differentiator(os.path.join(DIR, '../../examples/fortran/bimolkat/ffcn.f'))
+        backend_fortran = BackendFortran(os.path.join(DIR, '../../examples/fortran/bimolkat/libproblem.so'))
+        backend_pyadolc = BackendPyadolc(os.path.join(DIR, '../../examples/python/bimolkat/ffcn.py'))
+        ef = ExplicitEuler(backend_fortran)
+        ep = ExplicitEuler(backend_pyadolc)
+
+        ts          = numpy.linspace(0,2,50)
+        x0          = numpy.ones(5)
+        p           = numpy.ones(5)
+        q           = numpy.zeros((4, ts.size, 2))
+        q[0, :, 0]  = 90.
+        q[1:, :, 0] = 1.
+
+        P           = 1
+        x0_dot      = numpy.zeros((x0.size, P))
+        p_dot       = numpy.zeros((p.size, P))
+        q_dot       = numpy.zeros(q.shape + (P,))
+
+        p_dot[:, 0] = 1. 
+
+        ef.fo_forward_xpu(ts, x0, x0_dot, p, p_dot, q, q_dot)
+        xs_fortran = ef.xs.copy()
+        xs_dot_fortran = ef.xs_dot.copy()
+
+        ep.fo_forward_xpu(ts, x0, x0_dot, p, p_dot, q, q_dot)
+        xs_pyadolc = ep.xs.copy()
+        xs_dot_pyadolc = ep.xs_dot.copy()
+      
+        assert_almost_equal(xs_fortran, xs_pyadolc) 
+        assert_almost_equal(xs_dot_fortran, xs_dot_pyadolc) 
+
+
+    def test_pyadolc_fo_reverse(self):
+
+        backend_pyadolc = BackendPyadolc(os.path.join(DIR, '../../examples/python/bimolkat/ffcn.py'))
+        e = ExplicitEuler(backend_pyadolc)
+
+        ts          = numpy.linspace(0,2,50)
+        x0          = numpy.ones(5)
+        p           = numpy.ones(5)
+        q           = numpy.zeros((4, ts.size, 2))
+        q[0, :, 0]  = 90.
+        q[1:, :, 0] = 1.
+
+        P           = 1
+        x0_dot      = numpy.zeros((x0.size, P))
+        p_dot       = numpy.zeros((p.size, P))
+        q_dot       = numpy.zeros(q.shape + (P,))
+
+        p_dot[:, 0] = 1. 
+
+        e.fo_forward_xpu(ts, x0, x0_dot, p, p_dot, q, q_dot)
+
+        xs_bar = numpy.zeros(e.xs.shape)
+        xs_bar[-1,1] = 1.
+
+        e.fo_reverse(xs_bar)
+
+
+        a = numpy.sum(e.x0_bar * e.x0_dot[:,0]) + numpy.sum(e.p_bar * e.p_dot[:,0]) + numpy.sum(e.q_bar * e.q_dot[...,0])
+        b = numpy.sum(xs_bar * e.xs_dot[..., 0])
+
+        assert_almost_equal(a, b) 
+
 
 if __name__ == "__main__":
     run_module_suite()
